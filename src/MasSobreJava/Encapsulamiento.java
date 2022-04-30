936b111f2ec4fdd422107591f3e577a14469fc44
Thais C. Samudio A <samudioarauz@gmail.com>
2022-04-30 15:57:55 -0500

    práctica sobre encapsulamiento y creación de subclases

--- src/MasSobreJava/Encapsulamiento.java
+++ src/MasSobreJava/Encapsulamiento.java
@@ -2,10 +2,43 @@ package MasSobreJava;
 
 public class Encapsulamiento 
 {
-    
-public static void main(String[] args) 
-{
-    
-}
-    
+    private int edad;
+    private int hora;
+    private String nombre;
+    private String cumpleaños;
+
+
+    public static void main(String[] args) 
+    {
+        Encapsulamiento a = new Encapsulamiento();
+        Encapsulamiento.practica  b = a.new practica();
+        Encapsulamiento.practica.practicaza c = b.new practicaza();
+        
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    class practica{
+        practica(){
+            var a = new Encapsulamiento();
+            a.setEdad(23);
+            System.out.println(a.getEdad());
+            
+        }
+        class practicaza{
+            practicaza(){
+                var a = new Encapsulamiento();
+                a.setEdad(23);
+                System.out.println(a.getEdad());
+                
+            }
+        }
+    }
+
 }
